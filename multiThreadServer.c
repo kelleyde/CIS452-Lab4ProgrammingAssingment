b36648efe5d9d167a8e7270050f45da1bd2323a4
elle108 <ellemiller108@gmail.com>
2026-02-05 09:09:07 -0500

    Starting code

--- multiThreadServer.c
+++ multiThreadServer.c
@@ -1,1 +1,16 @@
 // File creation
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* findFile (void* arg);
+
+int main() {
+    pthread_t thread1;
+    char arg1[300];
+    scanf("%s", arg1);
+    thread1 = pthread_create (&thread1, NULL, findFile, (void*)arg1);
+}
