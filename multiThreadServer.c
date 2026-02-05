f27fd7f301f01ba094ce0909d65f2959977dc897
elle108 <ellemiller108@gmail.com>
2026-02-05 09:16:49 -0500

    FindFile implement

--- multiThreadServer.c
+++ multiThreadServer.c
@@ -14,3 +14,15 @@ int main() {
     scanf("%s", arg1);
     thread1 = pthread_create (&thread1, NULL, findFile, (void*)arg1);
 }
+
+void* findFile (void* arg) {
+    char* filename = (char*)arg;
+
+    if (rand() % 100 < 80) {
+        sleep(1);
+    } else {
+        sleep(7 + rand() % 4);
+    }
+    printf("File %s accessed\n", filename);
+    return NULL;
+}
