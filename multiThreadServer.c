5bb825d2d1fe09940412934459c4df97ab2c18c6
Delaney Kelley <kelleyde@mail.gvsu.edu>
2026-02-05 09:54:46 -0500

    exit gracefully

--- multiThreadServer.c
+++ multiThreadServer.c
@@ -5,14 +5,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <signal.h>
 
 void* findFile (void* arg);
+void sigHandler (int sigNum);
 
 int main() {
-    pthread_t thread1;
-    char arg1[300];
-    scanf("%s", arg1);
-    thread1 = pthread_create (&thread1, NULL, findFile, (void*)arg1);
+    while (1) {
+	void *result;
+    	pthread_t thread1;
+	
+	char arg1[300];	
+    //	char* arg1 = malloc[300];
+    	scanf("%s", arg1);
+    	thread1 = pthread_create (&thread1, NULL, findFile, (void*)arg1);
+	
+	// pthread_join(thread1, &result);
+	
+	signal (SIGINT, sigHandler);
+    }
+    
+    // free char* arg1;
+    return 0;
 }
 
 void* findFile (void* arg) {
@@ -25,4 +39,12 @@ void* findFile (void* arg) {
     }
     printf("File %s accessed\n", filename);
     return NULL;
+}
+
+void sigHandler (int sigNum)
+{
+ if(sigNum == SIGINT){
+ printf ("Exit Gracefully\n");
+ exit(0);
+ }
 }
