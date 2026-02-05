3deb59fd4d7f43e0b68216fd4c57c1e812514a39
elle108 <ellemiller108@gmail.com>
2026-02-05 13:53:40 -0500

    Elle changes

--- multiThreadServer.c
+++ multiThreadServer.c
@@ -9,27 +9,25 @@
 
 void* findFile (void* arg);
 void sigHandler (int sigNum);
+int fileRequests = 0;
 
 int main() {
-    while (1) {
-	void *result;
+    while(1){
     	pthread_t thread1;
-	
-	char arg1[300];	
-    //	char* arg1 = malloc[300];
+	    char* arg1 = malloc(300);	
     	scanf("%s", arg1);
-    	thread1 = pthread_create (&thread1, NULL, findFile, (void*)arg1);
-	
-	// pthread_join(thread1, &result);
-	
-	signal (SIGINT, sigHandler);
+        pthread_create(&thread1, NULL, findFile, (void*)arg1);
+        fileRequests++;
+        pthread_detach(thread1);
+        //free(arg1);
     }
-    
-    // free char* arg1;
+
+    signal (SIGINT, sigHandler);
     return 0;
 }
 
 void* findFile (void* arg) {
+    // pthread_detach(pthread_self());
     char* filename = (char*)arg;
 
     if (rand() % 100 < 80) {
